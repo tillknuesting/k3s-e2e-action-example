734a821dd99a5eb0ca3a8e5cc1e615f3653e4108
Till Knuesting <tillknuesting@gmail.com>
2022-11-29 11:56:54 +0000

    Update server.go

--- operator/server.go
+++ operator/server.go
@@ -9,7 +9,7 @@ import (
 )
 
 // A fundamental concept in `net/http` servers is
-// *handlers*. A handler is an object implementing the
+// *handlers*. A handler is an  object implementing the
 // `http.Handler` interface. A common way to write
 // a handler is by using the `http.HandlerFunc` adapter
 // on functions with the appropriate signature.
